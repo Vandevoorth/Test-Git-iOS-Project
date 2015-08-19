1dc9e68df7b499a549bd5a9924fa09b9862ad33b
Hendrik Vandevoort <Artlift@MacBook-Pro-van-Hendrik.local>
2015-08-19 08:33:18 +0200

    Initial Commit

---
+++ Git test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git test
+//
+//  Created by Hendrik Vandevoort on 19/08/15.
+//  Copyright (c) 2015 Mool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Git test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Git test
+//
+//  Created by Hendrik Vandevoort on 19/08/15.
+//  Copyright (c) 2015 Mool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
