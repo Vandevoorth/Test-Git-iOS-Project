c6f2ccd9da039b23cf2d3ecb98881d0bcc55cfc7
Hendrik Vandevoort <Artlift@MacBook-Pro-van-Hendrik.local>
2015-09-14 17:49:08 +0200

    first commit

---
+++ Git test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git test
+//
+//  Created by Hendrik Vandevoort on 14/09/15.
+//  Copyright (c) 2015 Mool. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git test
+//
+//  Created by Hendrik Vandevoort on 14/09/15.
+//  Copyright (c) 2015 Mool. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
